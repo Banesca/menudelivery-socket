8d9224fd3e8c90a01796f242fb8b6d0477e9efa4
JORGE GUTIERREZ <jorguti58@gmail.com>
2023-02-15 19:27:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ var io = require('socket.io')(serverhttps,{
   }
 })
 
-//io.origins(['https://menusoftware.info','https://menusoftware.info:80','https://menusoftware.info:443','http://localhost','http://localhost:80','http://72.167.55.26','http://72.167.55.26:2021','https://72.167.55.26:2021','https://api.menusoftware.info:2021','http://api.menusoftware.info','undefined']);
+io.origins(['https://menusoftware.info','https://menusoftware.info:80','https://menusoftware.info:443','http://localhost','http://localhost:80','http://72.167.55.26','http://72.167.55.26:2021','https://72.167.55.26:2021','https://api.menusoftware.info:2021','http://api.menusoftware.info','undefined']);
 
 
 io.on('connection', (socket) => {
